578efd316a6067870b6d8d4bc429f0b30ac749d1
umair-fiaz-tx <umair.fiaz@techcreatix.com>
2020-10-02 19:12:28 +0500

    commit for deploy to heroku

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module RailsProject
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = true
 
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
